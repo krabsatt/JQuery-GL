1e4b6b9ff060ce630e2208c21d9d377c4f8c2a93
krabsatt <krabsatt@hotmail.com>
2011-10-09 10:30:34 -0400

    .

--- bin/jquery.gl.js
+++ bin/jquery.gl.js
@@ -19,6 +19,33 @@
 // The .x object we add to the WebGLRenderContext.
 
 /**
+ * Creates a function that is called or adds a callback.
+ *
+ * @param {function} f  The initial function to call.
+ * @param {WebGLRenderContext} defaultGL  The default param when calling.
+ * @return Contructed method.
+ */
+function createAddOrCall(f, defaultGl) {
+  return function(optFunc) {
+    if (optFunc) {
+      var oldF = this[f];
+      this[f] = function(gl) {
+        oldF.apply(this, [gl]);
+        optFunc.apply(this, [gl])
+      };
+    } else {
+      this[f](defaultGl);
+    }
+    return this;
+  };
+}
+
+
+// Copyright (c) 2011 Kevin Rabsatt
+//
+// The .x object we add to the WebGLRenderContext.
+
+/**
  * Defines some shortcuts we will add to the context.
  * @constructor
  */
